36cc94106808f03a1e54f663b8200fbc06027286
leighshepperson <pmp07ls@gmail.com>
2016-05-27 23:16:36 +0100

    Refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 const NUMBER_OF_REGISTERS: usize = 3;
 
-#[derive(Debug)]
 enum Instruction {
     Halt,
-    Load {reg: usize, value: u16 },
-    Swap {reg1: usize, reg2: usize, reg3: usize},
-    Add {reg1: usize, reg2: usize, reg3: usize},
-    Branch {offset: usize}
+    Load { reg: usize, value: u16 },
+    Swap { reg1: usize, reg2: usize, reg3: usize },
+    Add { reg1: usize, reg2: usize, reg3: usize },
+    Branch { offset: usize }
 }
 
 fn main() {
-    let encoded_instructions = &[0x1110, 0x2100, 0x0, 0x0];
+    let encoded_instructions = &[0x1110, 0x2100, 0x3010, 0x0];
 
     cpu(encoded_instructions);
 }
@@ -20,53 +19,52 @@ fn cpu(encoded_instructions: &[u16]) {
     let mut reg_field = [0; NUMBER_OF_REGISTERS];
 
     loop {
-      let encoded_instruction = fetch(pc, encoded_instructions);
+        let encoded_instruction = fetch(pc, encoded_instructions);
+        let decoded_instruction = decode(encoded_instruction);
 
-      let decoded_instruction = decode(encoded_instruction);
+        match decoded_instruction {
+            Some(Instruction::Load { reg, value })
+                => load(reg, value, &mut reg_field, &mut pc),
+            Some(Instruction::Swap { reg1, reg2, reg3 })
+                => swap(reg1, reg2, reg3, &mut reg_field, &mut pc),
+            Some(Instruction::Add { reg1, reg2, reg3 })
+                => add(reg1, reg2, reg3, &mut reg_field, &mut pc),
+            Some(Instruction::Branch { offset })
+                => branch(offset, &mut pc),
+            Some(Instruction::Halt)
+                => { halt(&reg_field); break }
+            None => break,
+        }
+    }
+}
 
-      match decoded_instruction {
-          Some(Instruction::Load {reg, value})
-            => load(reg, value, &mut reg_field, &mut pc),
-          Some(Instruction::Swap {reg1, reg2, reg3})
-            => swap(reg1, reg2, reg3, &mut reg_field, &mut pc),
-          Some(Instruction::Add {reg1, reg2, reg3})
-            => add(reg1, reg2, reg3, &mut reg_field, &mut pc),
-          Some(Instruction::Branch {offset})
-            => branch(offset, &mut pc),
-          Some(Instruction::Halt)
-            => { halt(&reg_field); break; }
-          None => break,
-      }
-    };
+fn fetch(pc: usize, instructions: &[u16]) -> u16 {
+    instructions[pc]
 }
 
 fn halt(register_field: &[u16]) {
     println!("{:?}", register_field[0]);
 }
 
-fn load(register: usize, value: u16, register_field: &mut[u16], pc: &mut usize) {
+fn load(register: usize, value: u16, register_field: &mut [u16], pc: &mut usize) {
     register_field[register] = value;
     *pc += 1;
 }
 
-fn swap(reg1: usize, reg2: usize, reg3: usize, register_field: &mut[u16], pc: &mut usize) {
+fn swap(reg1: usize, reg2: usize, reg3: usize, register_field: &mut [u16], pc: &mut usize) {
     register_field[reg3] = register_field[reg1];
     register_field[reg1] = register_field[reg2];
     register_field[reg2] = register_field[reg3];
     *pc += 1;
 }
 
-fn add(reg1: usize, reg2: usize, reg3: usize, register_field: &mut[u16], pc: &mut usize) {
+fn add(reg1: usize, reg2: usize, reg3: usize, register_field: &mut [u16], pc: &mut usize) {
     register_field[reg3] = register_field[reg1] + register_field[reg2];
     *pc += 1;
 }
 
-fn branch(offset: usize, pc: &mut usize){
+fn branch(offset: usize, pc: &mut usize) {
     *pc -= offset;
-}
-
-fn fetch(pc: usize, instructions: &[u16]) -> u16 {
-    instructions[pc]
 }
 
 fn decode(encoded_instruction: u16) -> Option<Instruction> {
@@ -79,10 +77,10 @@ fn decode(encoded_instruction: u16) -> Option<Instruction> {
 
     match operator {
         0 => Some(Instruction::Halt),
-        1 => Some(Instruction::Load {reg: reg1, value: value}),
-        2 => Some(Instruction::Swap {reg1: reg1, reg2: reg2, reg3: reg3}),
-        3 => Some(Instruction::Add {reg1: reg1, reg2: reg2, reg3: reg3}),
-        4 => Some(Instruction::Branch {offset: offset}),
-        _ => None
+        1 => Some(Instruction::Load { reg: reg1, value: value }),
+        2 => Some(Instruction::Swap { reg1: reg1, reg2: reg2, reg3: reg3 }),
+        3 => Some(Instruction::Add { reg1: reg1, reg2: reg2, reg3: reg3 }),
+        4 => Some(Instruction::Branch { offset: offset }),
+        _ => None,
     }
 }
