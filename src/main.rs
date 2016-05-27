d94d558ec75f1d744dcf98643333809db44e5ab3
leighshepperson <pmp07ls@gmail.com>
2016-05-27 22:45:31 +0100

    Added instructions with exectue implementations

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,88 @@
-const NUMBER_OF_INSTRUCTIONS: usize = 4;
+const NUMBER_OF_REGISTERS: usize = 3;
 
 #[derive(Debug)]
-enum InstructionSet {
+enum Instruction {
     Halt,
-    Load {reg: i16, value: i16 },
-    Mov {reg1: i16, reg2: i16},
-    Add {reg1: i16, reg2: i16, reg3: i16}
+    Load {reg: usize, value: u16 },
+    Swap {reg1: usize, reg2: usize, reg3: usize},
+    Add {reg1: usize, reg2: usize, reg3: usize},
+    Branch {offset: usize}
 }
 
 fn main() {
-    let mut ic = 0;
-    let mut reg_field = [0; 2];
-    let mut running = true;
+    let encoded_instructions = &[0x1110, 0x2100, 0x0, 0x0];
 
-    let instructions = [0x3410, 0xF0, 0xFFF, 0x0];
+    cpu(encoded_instructions);
+}
 
-    while running {
+fn cpu(encoded_instructions: &[u16]) {
+    let mut pc = 0;
+    let mut reg_field = [0; NUMBER_OF_REGISTERS];
 
-      let instruction = fetch(ic, instructions);
+    loop {
+      let encoded_instruction = fetch(pc, encoded_instructions);
 
-      let decoded_instruction = decode(instruction);
+      let decoded_instruction = decode(encoded_instruction);
 
-      running = false;
-
-      println!("{:?}", decoded_instruction);
+      match decoded_instruction {
+          Some(Instruction::Load {reg, value})
+            => load(reg, value, &mut reg_field, &mut pc),
+          Some(Instruction::Swap {reg1, reg2, reg3})
+            => swap(reg1, reg2, reg3, &mut reg_field, &mut pc),
+          Some(Instruction::Add {reg1, reg2, reg3})
+            => add(reg1, reg2, reg3, &mut reg_field, &mut pc),
+          Some(Instruction::Branch {offset})
+            => branch(offset, &mut pc),
+          Some(Instruction::Halt)
+            => { halt(&reg_field); break; }
+          None => break,
+      }
     };
-
 }
 
-fn fetch(ic: usize, instructions: [i16; NUMBER_OF_INSTRUCTIONS]) -> i16 {
-    instructions[ic]
+fn halt(register_field: &[u16]) {
+    println!("{:?}", register_field[0]);
 }
 
-fn decode(instruction: i16) -> Option<InstructionSet> {
-    let instruction_number = instruction >> 12;
-    let reg1 = (instruction >> 8) & 0xF;
-    let reg2 = (instruction >> 4) & 0xF;
-    let reg3 = instruction & 0xF;
-    let value = instruction & 0xFF;
+fn load(register: usize, value: u16, register_field: &mut[u16], pc: &mut usize) {
+    register_field[register] = value;
+    *pc += 1;
+}
 
-    match instruction_number {
-        0 => Some(InstructionSet::Halt),
-        1 => Some(InstructionSet::Load {reg: reg1 , value: value}),
-        2 => Some(InstructionSet::Mov {reg1: reg1 , reg2: reg2}),
-        3 => Some(InstructionSet::Add {reg1: reg1, reg2: reg2, reg3: reg3}),
+fn swap(reg1: usize, reg2: usize, reg3: usize, register_field: &mut[u16], pc: &mut usize) {
+    register_field[reg3] = register_field[reg1];
+    register_field[reg1] = register_field[reg2];
+    register_field[reg2] = register_field[reg3];
+    *pc += 1;
+}
+
+fn add(reg1: usize, reg2: usize, reg3: usize, register_field: &mut[u16], pc: &mut usize) {
+    register_field[reg3] = register_field[reg1] + register_field[reg2];
+    *pc += 1;
+}
+
+fn branch(offset: usize, pc: &mut usize){
+    *pc -= offset;
+}
+
+fn fetch(pc: usize, instructions: &[u16]) -> u16 {
+    instructions[pc]
+}
+
+fn decode(encoded_instruction: u16) -> Option<Instruction> {
+    let operator = encoded_instruction >> 12;
+    let reg1 = ((encoded_instruction >> 8) & 0xF) as usize;
+    let reg2 = ((encoded_instruction >> 4) & 0xF) as usize;
+    let reg3 = (encoded_instruction & 0xF) as usize;
+    let offset = (encoded_instruction & 0xFFF) as usize;
+    let value = encoded_instruction & 0xFF;
+
+    match operator {
+        0 => Some(Instruction::Halt),
+        1 => Some(Instruction::Load {reg: reg1, value: value}),
+        2 => Some(Instruction::Swap {reg1: reg1, reg2: reg2, reg3: reg3}),
+        3 => Some(Instruction::Add {reg1: reg1, reg2: reg2, reg3: reg3}),
+        4 => Some(Instruction::Branch {offset: offset}),
         _ => None
     }
 }
