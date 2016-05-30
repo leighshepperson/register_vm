ff2789fb476728fa97d84506cc74bcb71576fd83
leighshepperson <pmp07ls@gmail.com>
2016-05-30 23:30:08 +0100

    Reworked

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,110 @@
-const NUMBER_OF_REGISTERS: usize = 3;
+type RegisterFile = [u16; 3];
 
+#[derive(Debug, Copy, Clone)]
 enum Instruction {
     Halt,
-    Load { reg: usize, value: u16 },
-    Swap { reg1: usize, reg2: usize, reg3: usize },
-    Add { reg1: usize, reg2: usize, reg3: usize },
+    Load { destination: usize, value: u16 },
+    Mov { reg1: usize, reg2: usize, reg3: usize },
+    Add { destination: usize, source: usize },
     Branch { offset: usize }
 }
 
-fn main() {
-    let encoded_instructions = &[0x1110, 0x2100, 0x3010, 0x0];
+impl Instruction {
+    fn decode(encoded_instruction: u16) -> Option<Self> {
+        let operator = encoded_instruction >> 12;
+        let reg1 = ((encoded_instruction >> 8) & 0xF) as usize;
+        let reg2 = ((encoded_instruction >> 4) & 0xF) as usize;
+        let reg3 = (encoded_instruction & 0xF) as usize;
+        let offset = (encoded_instruction & 0xFFF) as usize;
+        let value = encoded_instruction & 0xFF;
 
-    cpu(encoded_instructions);
-}
-
-fn cpu(encoded_instructions: &[u16]) {
-    let mut pc = 0;
-    let mut reg_field = [0; NUMBER_OF_REGISTERS];
-
-    loop {
-        let encoded_instruction = fetch(pc, encoded_instructions);
-        let decoded_instruction = decode(encoded_instruction);
-
-        match decoded_instruction {
-            Some(Instruction::Load { reg, value })
-                => load(reg, value, &mut reg_field, &mut pc),
-            Some(Instruction::Swap { reg1, reg2, reg3 })
-                => swap(reg1, reg2, reg3, &mut reg_field, &mut pc),
-            Some(Instruction::Add { reg1, reg2, reg3 })
-                => add(reg1, reg2, reg3, &mut reg_field, &mut pc),
-            Some(Instruction::Branch { offset })
-                => branch(offset, &mut pc),
-            Some(Instruction::Halt)
-                => { halt(&reg_field); break }
-            None => break,
+        match operator {
+            0 => Some(Instruction::Halt),
+            1 => Some(Instruction::Load { destination: reg1, value: value }),
+            2 => Some(Instruction::Mov { reg1: reg1, reg2: reg2, reg3: reg3 }),
+            3 => Some(Instruction::Add { destination: reg1, source: reg2 }),
+            4 => Some(Instruction::Branch { offset: offset }),
+            _ => None,
         }
     }
-}
 
-fn fetch(pc: usize, instructions: &[u16]) -> u16 {
-    instructions[pc]
-}
+    fn execute(&self, registers: &mut [u16], ic: &mut usize) -> bool {
+        match *self {
+            Instruction::Load { destination, value } => {
+                load(destination, value, registers);
+            },
+            Instruction::Mov { reg1, reg2, reg3 } => {
+                mov(reg1, reg2, reg3, registers);
+            },
+            Instruction::Add { destination, source } => {
+                add(destination, source, registers);
+            },
+            Instruction::Branch { offset } => {
+                branch(offset, ic);
+            },
+            Instruction::Halt => {
+                halt(registers);
+                return false;
+            },
+        }
 
-fn halt(register_field: &[u16]) {
-    println!("{:?}", register_field[0]);
-}
-
-fn load(register: usize, value: u16, register_field: &mut [u16], pc: &mut usize) {
-    register_field[register] = value;
-    *pc += 1;
-}
-
-fn swap(reg1: usize, reg2: usize, reg3: usize, register_field: &mut [u16], pc: &mut usize) {
-    register_field[reg3] = register_field[reg1];
-    register_field[reg1] = register_field[reg2];
-    register_field[reg2] = register_field[reg3];
-    *pc += 1;
-}
-
-fn add(reg1: usize, reg2: usize, reg3: usize, register_field: &mut [u16], pc: &mut usize) {
-    register_field[reg3] = register_field[reg1] + register_field[reg2];
-    *pc += 1;
-}
-
-fn branch(offset: usize, pc: &mut usize) {
-    *pc -= offset;
-}
-
-fn decode(encoded_instruction: u16) -> Option<Instruction> {
-    let operator = encoded_instruction >> 12;
-    let reg1 = ((encoded_instruction >> 8) & 0xF) as usize;
-    let reg2 = ((encoded_instruction >> 4) & 0xF) as usize;
-    let reg3 = (encoded_instruction & 0xF) as usize;
-    let offset = (encoded_instruction & 0xFFF) as usize;
-    let value = encoded_instruction & 0xFF;
-
-    match operator {
-        0 => Some(Instruction::Halt),
-        1 => Some(Instruction::Load { reg: reg1, value: value }),
-        2 => Some(Instruction::Swap { reg1: reg1, reg2: reg2, reg3: reg3 }),
-        3 => Some(Instruction::Add { reg1: reg1, reg2: reg2, reg3: reg3 }),
-        4 => Some(Instruction::Branch { offset: offset }),
-        _ => None,
+        true
     }
+}
+
+fn halt(register_file: &[u16]) {
+    println!("{:?}", register_file[0]);
+}
+
+fn load(destination: usize, value: u16, register_file: &mut [u16]) {
+    register_file[destination] = value;
+}
+
+fn mov(reg1: usize, reg2: usize, reg3: usize, register_file: &mut [u16]) {
+    register_file[reg3] = register_file[reg1];
+    register_file[reg1] = register_file[reg2];
+    register_file[reg2] = register_file[reg3];
+}
+
+fn add(destination: usize, source: usize, register_file: &mut [u16]) {
+    register_file[destination] = register_file[destination] + register_file[source];
+}
+
+fn branch(offset: usize, ic: &mut usize) {
+    *ic -= offset - 1;
+}
+
+struct Program<'a> {
+    instructions: &'a [u16],
+}
+
+impl<'a> Program<'a> {
+    fn fetch(&self, ic: usize) -> u16 {
+        self.instructions[ic]
+    }
+}
+
+fn cpu(program: Program) {
+    let mut ic = 0;
+    let mut registers = RegisterFile::default();
+
+    loop {
+        let encoded_instruction = program.fetch(ic);
+        let decoded_instruction = Instruction::decode(encoded_instruction);
+
+        match decoded_instruction {
+            Some(instr) => {
+                if !instr.execute(&mut registers, &mut ic) { break }
+            }
+            None => break,
+        }
+
+        ic += 1;
+    }
+}
+
+fn main() {
+    let encoded_instructions = Program { instructions: &[0x1110, 0x2100, 0x3010, 0x0] };
+
+    cpu(encoded_instructions);
 }
